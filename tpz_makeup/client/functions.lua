a7e4356a33290419b319053aa09bbf492ab71613
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-09-19 00:59:39 +0300

    Update functions.lua

--- tpz_makeup/client/functions.lua
+++ tpz_makeup/client/functions.lua
@@ -134,7 +134,7 @@ StartCam = function(x, y, z, rotx, roty, rotz, fov)
     local cameraHandler = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", x, y, z, rotx, roty, rotz, fov, true, 0)
 	SetCamActive(cameraHandler, true)
 	RenderScriptCams(true, true, 500, true, true)
-
+    return cameraHandler 
 end
 
 AdjustEntityPedHeading = function(amount)
